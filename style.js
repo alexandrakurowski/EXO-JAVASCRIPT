2cd05beb0ca133c8dbe17f415b81dfc7c566fbc2
alex <alex@SIMOCCAUCH21.lan>
2018-06-11 16:37:55 +0200

    multiplication

--- style.js
+++ style.js
@@ -10,9 +10,16 @@ alert("Nom: " + (nom) + "Prénom: "  + (prénom) + "Ville: " + (ville));
 var saisie = prompt(); 
 alert("Bonjour !\n" + "Prénom: " + saisie);
 */
-/* EXO 4*/
+/* EXO 4
 
 var nom = prompt("Saisir votre nom");
 var prénom = prompt("Saisir votre prénom\n");
 var ville = prompt("Saisir votre ville\n");	
-alert("Nom: " + (nom) + "\n" + "Prénom: "  + (prénom) + "\n" + "Ville: " + (ville));
+alert("Nom: " + (nom) + "\n" + "Prénom: "  + (prénom) + "\n" + "Ville: " + (ville));*/
+
+/* EXO 5 */
+
+var nb1 = prompt("saisir un nombre a virgule")
+var nb2 = prompt("saisir un deuxieme nombre à virgule")
+function multiply (nb1, nb2)
+{return nb1*nb2}
