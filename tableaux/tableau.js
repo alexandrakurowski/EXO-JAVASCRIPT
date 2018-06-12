658277b115c1aad8e47ddcc456013f256f20fc57
alex <alex@SIMOCCAUCH21.lan>
2018-06-12 13:44:33 +0200

    debut exo javascript mardi

---
+++ tableaux/tableau.js
@@ -0,0 +1,20 @@
+console.log ('hello')
+/*EX1*/
+var mois= ["janvier", "Février", "Mars", "Avril", "Mai", "juin", "Juillet", "Aout", "Septembre", "octobre", "Novembre", "Décembre"];
+ mois[7]="Août", 
+ 
+alert (mois);
+
+/*EX2*/
+
+alert (mois[2]);
+/*EX3*/
+
+alert(mois[5]); 
+/*EX4*/
+alert(mois[10]);
+/*EX5*/
+ mois[7]="Août", 
+alert(mois[7]); 
+
+
