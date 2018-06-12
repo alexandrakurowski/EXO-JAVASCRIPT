0776de5fb5eeffb49c71149b272fdf1a84c46d1e
alex <alex@SIMOCCAUCH21.lan>
2018-06-12 15:23:51 +0200

    fin des exos tableaux

--- tableaux/tableau.js
+++ tableaux/tableau.js
@@ -1,20 +1,33 @@
 console.log ('hello')
-/*EX1*/
+/*EX1
 var mois= ["janvier", "Février", "Mars", "Avril", "Mai", "juin", "Juillet", "Aout", "Septembre", "octobre", "Novembre", "Décembre"];
  mois[7]="Août", 
  
 alert (mois);
 
-/*EX2*/
+/*EX2
 
 alert (mois[2]);
-/*EX3*/
+/*EX3
 
 alert(mois[5]); 
-/*EX4*/
+/*EX4
 alert(mois[10]);
-/*EX5*/
+/*EX5
  mois[7]="Août", 
-alert(mois[7]); 
+alert(mois[7]);
+*/
+/*EX7A11*/
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+alert (array);
+array.push('Courgette');/*rajoute*/
+alert (array);
+array[4].push('Citron');
+alert (array);
+array.splice(1, 1);
+alert (array);
+array.splice(2, 0, 'poire');/*https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/splice*/
+alert (array);
 
-
+var fin = "C'est la fin !!!!!"
+alert (fin)
