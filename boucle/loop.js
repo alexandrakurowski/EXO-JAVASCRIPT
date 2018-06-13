94f8984cc26da277757fb26eb0b5d404ab1de4bf
alex <alex@SIMOCCAUCH21.lan>
2018-06-13 16:08:15 +0200

    "debut exo"

---
+++ boucle/loop.js
@@ -0,0 +1,29 @@
+console.log ("hello"); 
+var fin = "END"; 
+/*EXO1*/
+var nb = 0;
+
+for (nb = 0; nb  <= 10; nb++)
+ {
+	alert(nb);
+}
+/*EXO2*/
+var nb1 = 0; 
+var nb2 = nb;
+for (nb1 = 0; nb1 <= 20; nb1++){
+	var res = nb1 * nb2; 
+alert(res);	
+}
+
+/*EXO3*/
+var nb3 = 100; 
+var nb4 = prompt("saisir un nombre entre 1 et 100");
+for (nb3=100; nb3 >= 10; nb3--){
+ alert(res1);	var res1 = nb3 * nb4;	
+ }
+/*EXO4*/
+
+alert(fin); 
+
+ 
+
