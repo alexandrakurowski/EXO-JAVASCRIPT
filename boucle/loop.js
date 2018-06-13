f1e9e61bc22b8702bdc70dde6dce2a665b704e6d
alex <alex@SIMOCCAUCH21.lan>
2018-06-13 16:59:08 +0200

    fin de journéé

--- boucle/loop.js
+++ boucle/loop.js
@@ -1,13 +1,13 @@
 console.log ("hello"); 
 var fin = "END"; 
-/*EXO1*/
+/*EXO1
 var nb = 0;
 
 for (nb = 0; nb  <= 10; nb++)
  {
 	alert(nb);
 }
-/*EXO2*/
+/*EXO2
 var nb1 = 0; 
 var nb2 = nb;
 for (nb1 = 0; nb1 <= 20; nb1++){
@@ -15,13 +15,21 @@ for (nb1 = 0; nb1 <= 20; nb1++){
 alert(res);	
 }
 
-/*EXO3*/
+/*EXO3
 var nb3 = 100; 
 var nb4 = prompt("saisir un nombre entre 1 et 100");
 for (nb3=100; nb3 >= 10; nb3--){
- alert(res1);	var res1 = nb3 * nb4;	
- }
+ 	var res1 = nb3 * nb4;
+ 	alert(res1);	
+ }*/
 /*EXO4*/
+var nb5 = 1;
+for(nb5 = 1; nb5<=10; nb5 += nb5/2)
+{
+	alert(nb5);	
+}
+
+ 
 
 alert(fin); 
 
