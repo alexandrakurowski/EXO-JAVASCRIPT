f13d0f8217b16501166d89cc1707388181ab6810
alex <alex@SIMOCCAUCH21.lan>
2018-06-15 11:11:39 +0200

    fin exos

--- boucle/loop.js
+++ boucle/loop.js
@@ -22,14 +22,22 @@ for (nb3=100; nb3 >= 10; nb3--){
  	var res1 = nb3 * nb4;
  	alert(res1);	
  }*/
-/*EXO4*/
+/*EXO4*
 var nb5 = 1;
 for(nb5 = 1; nb5<=10; nb5 += nb5/2)
 {
 	alert(nb5);	
 }
-
- 
+/*EXO5
+var nb6 = 1; 
+for (nb6 = 1; <= 15; nb6++) {
+	alert(nb6   +  "On y arrive presque.");
+}
+/*EXO6*/
+var nb7 = 1
+for (nb7 = 1; nb7 <= 20; nb7--) {
+	alert (nb7   + "C'est presque bon."); 
+}
 
 alert(fin); 
 
